fb1f87ee3d6b1ca57cf79800c1891f7c3d5815e2
developerUser12-sudo <jebular481@gmail.com>
2025-05-03 19:29:46 +0200

    Proxy modificado (prueba)

--- backend/app/Http/Middleware/TrustProxies.php
+++ backend/app/Http/Middleware/TrustProxies.php
@@ -19,8 +19,6 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_FOR
-                        | Request::HEADER_X_FORWARDED_HOST
-                        | Request::HEADER_X_FORWARDED_PORT
-                        | Request::HEADER_X_FORWARDED_PROTO;
+    protected $headers = \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_ALL;
+
 }
