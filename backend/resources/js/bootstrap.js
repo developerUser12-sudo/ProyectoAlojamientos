387c7e076fccf131cadcb088775d35ee32bab779
developerUser12-sudo <jebular481@gmail.com>
2025-05-04 18:46:20 +0200

    Dockerfile para despliegue (prueba)

--- backend/resources/js/bootstrap.js
+++ backend/resources/js/bootstrap.js
@@ -8,9 +8,12 @@ window._ = _;
  */
 
 import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
