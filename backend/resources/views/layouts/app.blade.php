0bdc021e211e34252abb536fefa130c46695cafa
developerUser12-sudo <jebular481@gmail.com>
2025-05-08 22:39:31 +0200

    Retoques pagina principal y login

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -5,48 +5,39 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fuentes -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Cargar Bootstrap desde CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Cargar Bootstrap Icons desde CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Cargar tu CSS (si lo tienes) -->
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-
-    <!-- Scripts (para Bootstrap JS y otros) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Cargar Alpine.js -->
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2" defer></script>
 </head>
 
 <body>
     <div id="app">
         <header>
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <nav class="navbar navbar-expand-md bg-primary shadow-sm" id="customNavbar">
                 <div class="container-fluid">
-                    <!-- Logo con enlace a Angular -->
-                    <a class="navbar-brand d-flex flex-column align-items-center" href="https://proyectoalojamientos.onrender.com" routerLinkActive="active">
-                        <i class="bi bi-airplane"></i> Instaholidays
+                    <a class="navbar-brand d-flex flex-column align-items-center text-light fs-5 fw-semibold"
+                        href="http://localhost:4200/frontend/">
+                        <i class="bi bi-airplane-fill"></i> HolidaysNow
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarContent">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="navbar-nav ms-auto">
-                        <a class="nav-link" href="http://127.0.0.1:8000">Mi cuenta</a>
-
+                    <div class="collapse navbar-collapse" id="navbarContent">
+                        <div class="navbar-nav ms-auto d-flex flex-column flex-sm-row gap-2 gap-sm-4">
+                            <a class="nav-link text-light" href="http://localhost:4200/frontend/vuelos/">Vuelos</a>
+                            <a class="nav-link text-light" href="http://localhost:4200/frontend/hoteles/" >Hoteles</a>
+                            <a class="nav-link text-light" href="http://localhost:4200/frontend/vuelos/alquilercoches">Alquiler de coches</a>
+                            <a class="nav-link text-light" href="http://127.0.0.1:8000">Mi cuenta</a>
+                            <a class="nav-link text-light" href="http://127.0.0.1:8000">Soporte</a>
+                        </div>
                     </div>
                 </div>
             </nav>
@@ -57,7 +48,7 @@
         <main class="py-4">
             @yield('content')
         </main>
-        
+
     </div>
 </body>
 
