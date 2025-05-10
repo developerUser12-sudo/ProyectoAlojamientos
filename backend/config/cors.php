249ce7962264b727162be5315c3c517244ec9a5a
developerUser12-sudo <jebular481@gmail.com>
2025-05-10 22:14:59 +0200

    Cambio config en cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://holidaysnow.onrender.com/','http://localhost:4200/frontend'],
 
     'allowed_origins_patterns' => [],
 
