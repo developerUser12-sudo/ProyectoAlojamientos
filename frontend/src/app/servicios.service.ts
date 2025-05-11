7ff02d02c95a77343171e4209fba0a74c78f81ca
developerUser12-sudo <jebular481@gmail.com>
2025-05-11 20:22:20 +0200

    Urls dinamicas

--- frontend/src/app/servicios.service.ts
+++ frontend/src/app/servicios.service.ts
@@ -2,20 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from './user';
+import { environment } from '../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
-  private apiUrl = 'http://localhost:8000/api/coches';
-
+   private apiUrl = environment.apiUrl;
+ 
+  
   constructor(private http: HttpClient) { }
-
+  
+ 
   getCoches(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    return this.http.get(`${this.apiUrl}/api/coches`);
   }
   getUsuario() {
-    return this.http.get<User>('http://localhost:8000/username', { withCredentials: true });
+    return this.http.get<User>(`${this.apiUrl}/username`, { withCredentials: true });
   }
   
 
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://holidaysnowauth.onrender.com' // tu backend en Render
+};
