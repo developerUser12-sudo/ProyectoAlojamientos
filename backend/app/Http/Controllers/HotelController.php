63c2838defe8eee6c6c6fdc0c533e86a77e588ba
developerUser12-sudo <jebular481@gmail.com>
2025-06-16 17:51:01 +0200

    Prueba solucion error parametro

--- backend/app/Http/Controllers/HotelController.php
+++ backend/app/Http/Controllers/HotelController.php
@@ -29,7 +29,8 @@ class HotelController extends Controller
             if ($request->has('hora_cierre') && !empty($request->hora_cierre)) {
                 $queryHotel->whereRaw('hora_cierre like ?', ['%' . $request->hora_cierre . '%']);
             }
-            if ($request->has('comidas') && !empty($request->hora_cierre)) {
+            if ($request->has('comidas') && !empty($request->comidas)) {
+
                 $comidasString = $request->input('comidas', '');
                 $comidas = array_filter(array_map('trim', explode(',', $comidasString)));
 
